bc23701e5f7e161fdcf9b68b4d0031a902f83396
unknown <markowujanovic@gmail.com>
2025-04-25 12:55:17 +0200

    start hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,28 @@
+import search from '@/img/search.svg'
+
 const Hero = () => {
-	return <div></div>
+	console.log(search)
+
+	return (
+		<section id='hero'>
+			<div className='left'>
+				<h1>
+					Garantir <span>les droits</span> et <span>la dignité humaine</span> des personnes détenues
+				</h1>
+				<p>
+					Le Conseil central de surveillance pénitentiaire et 36 commissions de surveillance exercent une surveillance indépendante des prisons et du
+					traitement réservé aux personnes détenues.
+				</p>
+				<div className='search'>
+					<input type='text' placeholder='Cherchez un mot-clé' />
+					<button className='btn btn-icon'>
+						<img src={search.src} alt='Search' />
+					</button>
+				</div>
+			</div>
+			<div className='right'></div>
+		</section>
+	)
 }
 
 export default Hero
